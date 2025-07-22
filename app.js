4fb4697c25d09109c96bd5a4b8d9f9a697d9555f
jincy <jincya94@gmail.com>
2025-07-23 01:15:31 +0530

    project initialize

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const app=express()
+const env=require('dotenv').config()
+const connectDB=require('./config/db')
+connectDB()
+
+
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("server created");}
+)
